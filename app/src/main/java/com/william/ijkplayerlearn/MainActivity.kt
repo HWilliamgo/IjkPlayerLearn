ebccab0b0da1244469773006484c2fe443d5a44e
HWilliamgo <williamintoandroid@outlook.com>
2019-09-05 17:06:54 +0800

    可以简单地播放视频了

--- app/src/main/java/com/william/ijkplayerlearn/MainActivity.kt
+++ app/src/main/java/com/william/ijkplayerlearn/MainActivity.kt
@@ -3,20 +3,41 @@ package com.william.ijkplayerlearn
 import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.ViewGroup
+import com.blankj.utilcode.util.LogUtils
 import com.william.ijkvideoview.widget.media.IjkVideoView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val url=""
+    private val url =
+        ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val videoView = IjkVideoView(this)
-        fl_video_container.addView(videoView)
+        fl_video_container.addView(
+            videoView,
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+
+        videoView.setOnErrorListener { iMediaPlayer, i, i2 ->
+            LogUtils.e("onError i=$i, i2=$i2")
+            return@setOnErrorListener false
+        }
+        videoView.setOnPreparedListener {
+            LogUtils.d("onPrepared")
+        }
+        videoView.setOnInfoListener { iMediaPlayer, i, i2 ->
+            LogUtils.d("onInfo i=$i, $i2")
+            return@setOnInfoListener false
+        }
 
         videoView.setVideoURI(Uri.parse(url))
+        videoView.start()
+
     }
 }
